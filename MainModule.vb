786f3b3c558928478027769aeb9501e1191855d5
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-05-15 20:40:29 -0700

    Saving for reboot.
    Pluralsight not playing video

--- MainModule.vb
+++ MainModule.vb
@@ -1,6 +1,17 @@
 ﻿Module MainModule
 
     Sub Main()
+        Dim ProductID As Integer
+        Dim isActive As Boolean
+        Dim Name As String
+        Dim ListPrice As Decimal
+        Dim SellStartDate As DateTime
+
+        ProductID = 1
+        isActive = True
+        Name = "10 Speed Bike"
+        ListPrice = 999.99D
+        SellStartDate = #01/01/2019#
 
     End Sub
 
