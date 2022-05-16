36038c8268f950884dd1b65264d7cd3eda1c8ee1
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-05-15 22:18:22 -0700

    save and close

--- MainModule.vb
+++ MainModule.vb
@@ -2,11 +2,11 @@
     Dim Name As String = "Mountain Bike"
 
     Sub Main()
-        Dim isActive As Boolean = DEFAULT_ACTIVE
+        Dim isActive As Boolean = ClassConstants.DEFAULT_ACTIVE
         Dim Name As String = "10 Speed Bike"
         Dim ListPrice As Decimal
 
-        ListPrice = DEFAULT_LIST_PRICE
+        ListPrice = ClassConstants.DEFAULT_LIST_PRICE
 
         Console.ReadKey()
     End Sub
