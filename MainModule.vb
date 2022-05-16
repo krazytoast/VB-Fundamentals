d598b1924d78b6451c99ee129423b284da8f595b
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-05-15 21:58:00 -0700

    Added ModuleContsants
    Added a Module for Constants to test public and private constants

--- MainModule.vb
+++ MainModule.vb
@@ -2,22 +2,16 @@
     Dim Name As String = "Mountain Bike"
 
     Sub Main()
-        Console.WriteLine(Name)
+        If True Then
+            Dim Name As String = "Tricycle"
+            Dim ListPrice As Decimal = 59.99
+        End If
 
+        Console.WriteLine(Name)
+        ' The following does not compile
+        ' Console.WriteLine(ListPrice)
 
         Console.ReadKey()
-
-
-
-        ' This is a Comment. Ctrl+k+c to comment highlighted line(s). Ctrl+k+u to uncomment selection
-        'Dim ProductID As Integer = 1
-        'Dim isActive As Boolean = True
-        'Dim Name As String = "10 Speed Bike"
-        'Dim ListPrice As Decimal = 999.99D
-        'Dim SellStartDate As DateTime = #01/01/2022#
-
-
-
     End Sub
 
 End Module
