d67767c3e5eddfb2879985205351c7976e2d413a
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-05-15 21:33:40 -0700

    Create Sub Main
    created sub main with console writeline & readkay

--- MainModule.vb
+++ MainModule.vb
@@ -1,17 +1,22 @@
 ﻿Module MainModule
+    Dim Name As String = "Mountain Bike"
 
     Sub Main()
-        Dim ProductID As Integer
-        Dim isActive As Boolean
-        Dim Name As String
-        Dim ListPrice As Decimal
-        Dim SellStartDate As DateTime
+        Console.WriteLine(Name)
 
-        ProductID = 1
-        isActive = True
-        Name = "10 Speed Bike"
-        ListPrice = 999.99D
-        SellStartDate = #01/01/2019#
+
+        Console.ReadKey()
+
+
+
+        ' This is a Comment. Ctrl+k+c to comment highlighted line(s). Ctrl+k+u to uncomment selection
+        'Dim ProductID As Integer = 1
+        'Dim isActive As Boolean = True
+        'Dim Name As String = "10 Speed Bike"
+        'Dim ListPrice As Decimal = 999.99D
+        'Dim SellStartDate As DateTime = #01/01/2022#
+
+
 
     End Sub
 
